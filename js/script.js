fab5c2f2e6e4a64861a7776d7691523660f00ce7
tengkiatwong <tengkiatwong@gmail.com>
2015-08-16 19:56:32 +0800

    navbar

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var myApp = angular.module('mvmt',['ngRoute']);
+
+myApp.config(function($routeProvider){
+    
+    $routeProvider
+    
+    .when('/',{
+        templateUrl:'/pages/home.html',
+        controller:'mainController'
+    })
+    
+    .when('/login',{
+        templateUrl:'/pages/login.html',
+        controller:'loginController'
+        
+    })
+    
+    .when('/help',{
+        templateUrl:'/pages/help.html',
+        controller:'mainController'
+        
+    })
+    
+    .when('/catalogue',{
+        templateUrl:'/pages/catalogue',
+        controller:'catalogueController'
+    })
+    
+    .when('/Faq',{
+        templateUrl:'/pages/Faq',
+        controller:'catalogueController'
+        
+    })
+    
+    .when('/about',{
+        templateUrl:'/pages/about'
+    })
+});
+    
+    
